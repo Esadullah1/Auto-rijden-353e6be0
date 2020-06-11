f7ee5e6721ed768764c68673e126cfcf2e228cf0
Esadullah Bagci <2083226@talnet.nl>
2020-06-11 12:06:06 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,21 @@
+<?php 
+
+		
+
+	
+        $line1 = readline("Hoe oud ben je?");
+        
+     	
+        if ($line1 < 16.50) {
+        	echo "Helaas, je mag nog niet beginnen met rijlessen";
+        }
+
+        else{
+        	echo "Je mag beginnen met rijlessen";
+        }
+
+
+
+
+
+ ?>
